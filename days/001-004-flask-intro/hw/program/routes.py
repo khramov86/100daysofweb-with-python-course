f4c4a4165a2720c2e03672765b51d0ddcad1c606
vkhramov <khramov.vladimir@gmail.com>
2022-12-13 21:09:57 +0300

    upd

--- days/001-004-flask-intro/hw/program/routes.py
+++ days/001-004-flask-intro/hw/program/routes.py
@@ -1,6 +1,11 @@
 from program import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello Pythonistas"
+    return render_template('index.html')
+
+@app.route('/100Days')
+def hundred_days():
+    return render_template('100days.html')
